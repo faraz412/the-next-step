2e3936c7fdd4c0e13e57fb4c11e576d1397023c0
faraz412 <ahmedfaraz412@gmail.com>
2023-06-02 21:28:33 +0530

    event path bug fixed in login and register page

--- frontend/functionalities/login.js
+++ frontend/functionalities/login.js
@@ -6,18 +6,18 @@ const logBtn1=document.querySelector(".default-a");
 logBtn1.addEventListener("click",(e)=>{
     //console.log(e);
     e.target.classList.add("click_effect");
-    e.path[1].children[1].classList.remove("click_effect");
-    e.path[2].children[4].classList.add("form_hide");
-    e.path[2].children[3].classList.remove("form_hide");    
+    e.composedPath()[1].children[1].classList.remove("click_effect");
+    e.composedPath()[2].children[4].classList.add("form_hide");
+    e.composedPath()[2].children[3].classList.remove("form_hide");    
 })
 
 const logBtn2=document.querySelector(".default-b");
 logBtn2.addEventListener("click",(e)=>{
     //console.log(e);
     e.target.classList.add("click_effect");
-    e.path[1].children[0].classList.remove("click_effect");
-    e.path[2].children[3].classList.add("form_hide");
-    e.path[2].children[4].classList.remove("form_hide");    
+    e.composedPath()[1].children[0].classList.remove("click_effect");
+    e.composedPath()[2].children[3].classList.add("form_hide");
+    e.composedPath()[2].children[4].classList.remove("form_hide");    
 })
 
 
@@ -43,11 +43,15 @@ userLogForm.addEventListener("submit", async (e)=>{
         })
         if(res.ok){
             let data=await res.json();
-            console.log(data);
-            localStorage.setItem("token",data.token);
-            localStorage.setItem("name",data.userName);
-            alert(data.msg);
-            window.location.href="../index.html";
+            //console.log(data);
+            if(data.userName){
+                localStorage.setItem("token",data.token);
+                localStorage.setItem("name",data.userName);
+                alert(data.msg);
+                window.location.href="../index.html";
+            }else{
+                alert(data.msg);
+            }
         }
     }catch(err){
         alert(err);
@@ -73,10 +77,14 @@ adminLogForm.addEventListener("submit", async (e)=>{
         if(res.ok){
             let data=await res.json();
             //console.log(data);
-            localStorage.setItem("token",data.token);
-            localStorage.setItem("admin",data.admin);
-            alert(data.msg);
-            window.location.href="dash.html";
+            if(data.admin){
+                localStorage.setItem("token",data.token);
+                localStorage.setItem("admin",data.admin);
+                alert(data.msg);
+                window.location.href="dash.html";
+            }else{
+                alert(data.msg);
+            }
         }
     }catch(err){
         aler(err);
